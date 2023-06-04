d3293a0627ea64ea87ff12b2e6814c67678895c1
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2023-06-05 00:32:00 +0800

    ci(package.json): 集成规范相关脚本

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="outer">
+    <div class="inner">fffff</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.outer {
+  .inner {
+    color: #ccc;
+  }
+}
+</style>
