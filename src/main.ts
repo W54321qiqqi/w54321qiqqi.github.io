67dc54302e07bb21ed4b24cf3b1fbf9ce564cd0f
qyh <tuhui@thinkerx.com>
2023-06-20 16:46:03 +0800

    chore: change file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from './App.vue'
 import { registerIcons } from '/@/utils/common'
 import router from '/@/router'
 import '/@/styles/index.scss'
-// 引入样式清除
-import 'normalize.css/normalize.css'
 function start() {
   const app = createApp(App)
   registerIcons(app) // icons
